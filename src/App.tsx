5d5092a31d19eb78eee7d21e16238446e12ac8b4
Aditya Sen <adityasen120@gmail.com>
2023-05-04 22:05:04 -0400

    basic layout + create set home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import SetHome from './components/SetHome/SetHome';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <div className="container text-center my-2">
+        <div className="row">
+          <div className="col">
+            <SetHome />
+          </div>
+          <div className="col">
+            <SetHome />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <SetHome />
+          </div>
+          <div className="col">
+            <SetHome />
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/SetHome/SetHome.tsx
@@ -0,0 +1,11 @@
+function SetHome() {
+    return (
+        <div className="card m-2">
+            <div className="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    )
+}
+
+export default SetHome;
